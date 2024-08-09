e52d693dbfc800481ae4341ff5165a8096a79672
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-08-09 11:30:23 -0300

    test: correção de erro

--- cypress/e2e/singup.cy.ts
+++ cypress/e2e/singup.cy.ts
@@ -8,13 +8,12 @@ describe('Sign up', () => {
   describe('Sign up', () => {
     const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
     const password = Cypress.env('USER_PASSWORD')
-  
-    it('successfully signs up using confirmation code sent via email', () => {
+
+    it('successfully login using confirmation code sent via email', () => {
       cy.fillSignupFormAndSubmit(emailAddress, password)
-  
+
       cy.contains('h1', 'Your Notes').should('be.visible')
       cy.contains('a', 'Create a new note').should('be.visible')
     })
   })
-
 })
