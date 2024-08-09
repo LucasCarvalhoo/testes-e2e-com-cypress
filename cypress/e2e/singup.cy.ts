0ded803518b89d703fcb4e391680181f0b408a0b
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-08-09 10:29:22 -0300

    test: refatorando o teste singup com comando customizado

--- cypress/e2e/singup.cy.ts
+++ cypress/e2e/singup.cy.ts
@@ -3,26 +3,18 @@
 import { faker } from '@faker-js/faker/locale/en'
 
 describe('Sign up', () => {
-  it('successfully signs up using confirmation code sent via email', () => {
+  // cypress/e2e/signup.cy.js
+
+  describe('Sign up', () => {
     const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
     const password = Cypress.env('USER_PASSWORD')
-
-    cy.intercept('GET', '**/notes').as('getNotes')
-    cy.visit('/signup')
-    cy.get('#email').type(emailAddress)
-    cy.get('#password').type(password, { log: false })
-    cy.get('#confirmPassword').type(password, { log: false })
-    cy.contains('button', 'Signup').click()
-    cy.get('#confirmationCode').should('be.visible')
-
-    cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
-      sentTo: emailAddress
-    }).then(message => {
-      const confirmationCode = message.html.body.match(/\d{6}/)[0]
-      cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
-
-      cy.wait('@getNotes')
+  
+    it('successfully signs up using confirmation code sent via email', () => {
+      cy.fillSignupFormAndSubmit(emailAddress, password)
+  
       cy.contains('h1', 'Your Notes').should('be.visible')
+      cy.contains('a', 'Create a new note').should('be.visible')
     })
   })
+
 })
