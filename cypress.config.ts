331aaff5a0d54de60635e0feb34bb4d099268382
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-08-09 10:16:33 -0300

    feat: configurando base url do mailosaur

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  viewportHeight: 880,
-  viewportWidth: 1280,
-
   e2e: {
+    baseUrl: 'https://notes-serverless-app.com',
     setupNodeEvents(on, config) {
     },
   },
